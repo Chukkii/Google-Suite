c9516926f881e8d7181da74d9a1b9226f1d97796
Bruce Choi <choi.bruce@gmail.com>
2012-01-22 18:25:56 +1100

    Initial Setup

---
+++ GoogleSuite/GoogleSuite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.GData.Calendar;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using GoogleLibrary;
+
+namespace GoogleSuite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private CalendarManager _calendarManager = new CalendarManager("Bruce@Brucechoi.com", "stuffis11");
+
+        public ActionResult Index()
+        {
+            EventFeed eventFeed = _calendarManager.GetAllCalendarEvents();
+            List<EventEntry> entries = new List<EventEntry>();
+
+            foreach (EventEntry eventEntry in eventFeed.Entries)
+            {
+                entries.Add(eventEntry);
+            }
+
+            return View(entries);
+        }
+
+    }
+}
